3e49dcd6a273da8af978c32a460acdce6f0fcc85
Alexander Zinin <spore_09@mail.ru>
2023-10-16 13:47:36 +0400

    rework file randomization logic

--- src/images/variants/ImageAlwaysRandomVariant.ts
+++ src/images/variants/ImageAlwaysRandomVariant.ts
@@ -1,25 +1,31 @@
-import { bound, randInt } from '../../utils/math';
+import { randInt } from '../../utils/math';
 import { Image, ImageBinary } from '../types';
 import { IImageVariant, ImageFormat } from './types';
 import sharp from 'sharp';
 
 export class ImageAlwaysRandomVariant implements IImageVariant {
+  private readonly randomSquareEdgeSizePx = 16;
+
   constructor(private readonly outputFormat: ImageFormat) {}
 
   async generate(image: Image): Promise<ImageBinary> {
-    console.count('ImageOriginalVariant.generate');
     const sharpImage = sharp(image.binary.buffer);
 
     const metadata = await sharpImage.metadata();
     const buffer = await sharpImage.raw().toBuffer();
 
-    for (let i = 0; i < buffer.length; i++) {
-      if (metadata.channels === 4 && i % 4 === 3) {
-        continue;
-      }
+    for (let y = 0; y < this.randomSquareEdgeSizePx; y++) {
+      for (let x = 0; x < this.randomSquareEdgeSizePx; x++) {
+        const offset =
+          y * metadata.width! * metadata.channels! + x * metadata.channels!;
+        buffer[offset + 0] = randInt(0, 255);
+        buffer[offset + 1] = randInt(0, 255);
+        buffer[offset + 2] = randInt(0, 255);
 
-      const r = randInt(-10, 10);
-      buffer[i] = bound(buffer[i] + r, 0, 255);
+        if (metadata.channels === 4) {
+          buffer[offset + 3] = 255;
+        }
+      }
     }
 
     const result = await sharp(buffer, {
